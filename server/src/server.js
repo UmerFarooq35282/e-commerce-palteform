702941f9eabf86005a054e4432eb8696d8792b9b
UmerFarooq35282 <umerfarooq2821035@gmail.com>
2026-07-03 21:43:22 +0500

    feat(database): configure MongoDB connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,23 @@
 import "dotenv/config.js"
 import { app } from "./app.js";
+import connectDatabase from "./config/database.js";
 
 const PORT = process.env.PORT || 3000
 
+const startServer = async () => {
+    try {
+        await connectDatabase();
 
-app.listen(PORT , () => {
-    console.log("Server is running on PORT || " , PORT);
-    
-})
+        app.listen(PORT, () => {
+            console.log("Server is running on PORT || ", PORT);
+
+        })
+
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+}
+
+
+startServer();
