5a8b4ee960cfc923f2535c7847c95bf56985aee8
UmerFarooq35282 <umerfarooq2821035@gmail.com>
2026-07-03 22:04:50 +0500

    feat(database): complete database foundation setup

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import "dotenv/config.js"
 import { app } from "./app.js";
 import connectDatabase from "./config/database.js";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 3000
 
@@ -12,6 +13,17 @@ const startServer = async () => {
             console.log("Server is running on PORT || ", PORT);
 
         })
+
+        process.on("SIGINT", async () => {
+            console.log("\nShutting down server...");
+
+            await mongoose.connection.close();
+
+            server.close(() => {
+                console.log("Server closed.");
+                process.exit(0);
+            });
+        });
 
     } catch (error) {
         console.error(error.message);
